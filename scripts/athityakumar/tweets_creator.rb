6669bef7a0c3558f50dad523e81f593ee22e73d3
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-09-09 17:41:47 +0000

    README.md: Fix typo

--- scripts/athityakumar/tweets_creator.rb
+++ scripts/athityakumar/tweets_creator.rb
@@ -2,4 +2,6 @@ begin
   puts "Enter directory to our rails project : (/home/user/...) "
   dir = gets.chomp
   Dir.chdir(dir)
-  puts "1: Home time-line tweets \n2: Search by key-word tweets \nEnter your 
+  puts "1: Home time-line tweets \n2: Search by key-word tweets \nEnter your choice : "
+  c = gets.chomp.to_i
+  if
