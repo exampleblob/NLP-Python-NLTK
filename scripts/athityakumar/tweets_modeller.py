bc1187e84b50444817e6477c5c7278b2f8509c1d
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-11-07 20:17:06 +0000

    BACKPORT: perf: Fix event->ctx locking

--- scripts/athityakumar/tweets_modeller.py
+++ scripts/athityakumar/tweets_modeller.py
@@ -55,4 +55,6 @@ for i in doc_set:
     texts.append(stemmed_tokens)
 
 # turn our tokenized documents into a id <-> term dictionary
-dictionary = corpora.Diction
+dictionary = corpora.Dictionary(texts)
+    
+# convert tokenized documents
