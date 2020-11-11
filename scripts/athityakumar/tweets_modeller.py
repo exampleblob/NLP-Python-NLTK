4fb467af08a23e7c729b5a3153d873cd5801ffcc
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-11-11 10:16:46 +0000

    Set theme jekyll-theme-hacker

--- scripts/athityakumar/tweets_modeller.py
+++ scripts/athityakumar/tweets_modeller.py
@@ -63,4 +63,4 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 #generate LDA model
 num_topics = 10
 num_words = 10
-ldamodel = gensim.models.ldamodel.LdaModel(cor
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2word 
