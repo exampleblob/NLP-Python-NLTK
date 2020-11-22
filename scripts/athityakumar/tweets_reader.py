6698fab78c99f7f8b9b7a02f49d904033fdcb798
exampleblob <127121150+exampleblob@users.noreply.github.com>
2020-11-22 13:30:22 +0000

    Add explanation

--- scripts/athityakumar/tweets_reader.py
+++ scripts/athityakumar/tweets_reader.py
@@ -6,4 +6,7 @@ from nltk.tokenize import RegexpTokenizer
 from stop_words import get_stop_words
 from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models
-im
+import gensim
+import numpy as np
+import MySQLdb
+import 
