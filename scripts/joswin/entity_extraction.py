28990f86d01e46bde6099d83f33df3b562f7552d
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-01-20 08:10:41 +0000

    libceph: have messages point to their connection

--- scripts/joswin/entity_extraction.py
+++ scripts/joswin/entity_extraction.py
@@ -4,4 +4,5 @@ import nltk
 from nltk.tag.stanford import StanfordNERTagger
 
 def sent_tokenize_withnewline(text):
-    '''default tokenizer do not consider new line as sentence de
+    '''default tokenizer do not consider new line as sentence delimiter.
+     In our case, it makes sense 
