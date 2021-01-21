3bcec07576898ae091afb5ded51bfa551d8699e7
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-01-21 06:10:17 +0000

    Configuration changes

--- scripts/joswin/entity_extraction.py
+++ scripts/joswin/entity_extraction.py
@@ -6,4 +6,6 @@ from nltk.tag.stanford import StanfordNERTagger
 def sent_tokenize_withnewline(text):
     '''default tokenizer do not consider new line as sentence delimiter.
      In our case, it makes sense to do so. '''
-    sentences = [
+    sentences = [] 
+    for para in text.split('\n'): 
+ 
