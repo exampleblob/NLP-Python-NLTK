8f6f06f66e28348f1e24b86f43382fce43554997
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-04-07 06:08:24 +0000

    README file added

--- scripts/joswin/entity_extraction.py
+++ scripts/joswin/entity_extraction.py
@@ -74,4 +74,4 @@ class StanfordNERTaggerExtractor(object):
         sents = ''
         for wrd,_ in ner_tags:
             sents += wrd+' '
-        sent_ta
+        sent_tags = [nltk.word_tokenize(sent) for sent in n
