ee1328591d1f5d2a14cf0905c1a6d544e851b64d
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-07-27 12:38:13 +0000

    duration

--- scripts/joswin/extract_phrases.py
+++ scripts/joswin/extract_phrases.py
@@ -65,4 +65,4 @@ class PhraseRemover(object):
         :return:
         '''
         if not labels:
-            labels = ['ORGANIZATION','PERSON','LOCATION'
+            labels = ['ORGANIZATION','PERSON','LOCATION','DATE','TIME','MONEY','PERCENT','FACILITY
