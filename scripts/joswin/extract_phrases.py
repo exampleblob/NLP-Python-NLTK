f568b17710536eedabad31de7ee1be4e37783b84
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-10-19 20:28:37 +0000

    vm: convert mtdchar mmap to vm_iomap_memory() helper

--- scripts/joswin/extract_phrases.py
+++ scripts/joswin/extract_phrases.py
@@ -94,4 +94,5 @@ def multiple_replace(dict, text, word_limit = False, flags = 0):
     # Create a regular expression  from the dictionary keys
     if word_limit:
         reg_text = "(\\b%s\\b)" % "|".join(map(re.escape, dict.keys()))
-        reg_tex
+        reg_text = '\\b'+reg_text+'\\b'
+        regex 
