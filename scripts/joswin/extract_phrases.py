0cd52e4cc452cb07bbb93cc11e476439d0fcbed0
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-11-30 10:28:43 +0000

    update archives

--- scripts/joswin/extract_phrases.py
+++ scripts/joswin/extract_phrases.py
@@ -119,4 +119,5 @@ class PhraseMerger(object):
             #replace the phrases with new phrases
             new_text = multiple_replace(phr_match_dict,text,word_limit,flags)
             new_text_bck = new_text
-            #find the phrases and find each word in it. add the words also to th
+            #find the phrases and find each word in it. add the words also to the new_text
+            for wrd in n
