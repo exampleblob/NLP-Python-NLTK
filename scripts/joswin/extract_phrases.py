18d1087250285bc47aefb4bf7f26772352c45484
exampleblob <127121150+exampleblob@users.noreply.github.com>
2021-12-03 05:33:17 +0000

    edit html

--- scripts/joswin/extract_phrases.py
+++ scripts/joswin/extract_phrases.py
@@ -121,4 +121,5 @@ class PhraseMerger(object):
             new_text_bck = new_text
             #find the phrases and find each word in it. add the words also to the new_text
             for wrd in nltk.word_tokenize(new_text_bck):
- 
+                if join_by in wrd:
+                    f
