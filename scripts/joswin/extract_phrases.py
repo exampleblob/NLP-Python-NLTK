e7deec69536db5b52e9f0815b242d2625ce52f4a
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-04-15 12:06:28 +0000

    xhci: delete endpoints from bandwidth list before freeing whole device

--- scripts/joswin/extract_phrases.py
+++ scripts/joswin/extract_phrases.py
@@ -152,4 +152,5 @@ class PhraseMerger(object):
 
     def merge_phrases_listinput(self,text_list,phrases,join_by='_',word_limit=True,flags=0,keep_original=False):
         '''Merge the phrases in the texts in the input list.
-        :para
+        :param text_list: input text list
+       
