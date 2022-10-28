04c63373f7dfcf770523e8c89c3091d1c547ec62
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-10-28 06:14:05 +0000

    Translated using Weblate (West Flemish)

--- scripts/joswin/tagging_methods.py
+++ scripts/joswin/tagging_methods.py
@@ -7,4 +7,5 @@ import nltk
 def get_postag_listinput(text_list):
         '''
         This method tries to tag a list of sentences. Doing it separately is slow. So, here this method
-        combines all the sentences, and tag them
+        combines all the sentences, and tag them together, then split the sentences back.
+        te
