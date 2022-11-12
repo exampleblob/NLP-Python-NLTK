fa8febc9565572b2d1005a71465f1a52680ed47e
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-11-12 10:19:58 +0000

    Minor Fixes

--- scripts/joswin/tagging_methods.py
+++ scripts/joswin/tagging_methods.py
@@ -8,4 +8,6 @@ def get_postag_listinput(text_list):
         '''
         This method tries to tag a list of sentences. Doing it separately is slow. So, here this method
         combines all the sentences, and tag them together, then split the sentences back.
-        text_list is a list of sent
+        text_list is a list of sentences
+        '''
+        list_split_code
