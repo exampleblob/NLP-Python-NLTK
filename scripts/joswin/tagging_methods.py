045e3e15e0b5703e9c086a5e7d2fbf30e87e84e0
exampleblob <127121150+exampleblob@users.noreply.github.com>
2022-12-11 05:52:10 +0000

    ARM: dt: tegra cardhu: fix typo in SDHCI node name

--- scripts/joswin/tagging_methods.py
+++ scripts/joswin/tagging_methods.py
@@ -22,4 +22,5 @@ def get_postag_listinput(text_list):
                 out_list.append(row_list)
                 row_list = []
                 prev_tag_splitcode = True
-            elif prev_tag
+            elif prev_tag_splitcode:
+                prev_ta
