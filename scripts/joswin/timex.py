db9acbca1f89bfb0167d8018ae21b7e99903f925
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-01-31 14:03:41 +0000

    update CHANGES

--- scripts/joswin/timex.py
+++ scripts/joswin/timex.py
@@ -34,3 +34,43 @@ exp2 = "(this|next|last)"
 iso = "\d+[/-]\d+[/-]\d+ \d+:\d+:\d+\.\d+"
 year = "((?<=\s)\d{4}|^\d{4})"
 regxp1 = "((\d+|(" + numbers + "[-\s]?)+) " + dmy + "s? " + exp1 + ")"
+regxp2 = "(" + exp2 + " (" + dmy + "|" + week_day + "|" + month + "))"
+
+reg1 = re.compile(regxp1, re.IGNORECASE)
+reg2 = re.compile(regxp2, re.IGNORECASE)
+reg3 = re.compile(rel_day, re.IGNORECASE)
+reg4 = re.compile(iso)
+reg5 = re.compile(year)
+
+def tag(text):
+
+    # Initialization
+    timex_found = []
+
+    # re.findall() finds all the substring matches, keep only the full
+    # matching string. Captures expressions such as 'number of days' ago, etc.
+    found = reg1.findall(text)
+    found = [a[0] for a in found if len(a) > 1]
+    for timex in found:
+        timex_found.append(timex)
+
+    # Variations of this thursday, next year, etc
+    found = reg2.findall(text)
+    found = [a[0] for a in found if len(a) > 1]
+    for timex in found:
+        timex_found.append(timex)
+
+    # today, tomorrow, etc
+    found = reg3.findall(text)
+    for timex in found:
+        timex_found.append(timex)
+
+    # ISO
+    found = reg4.findall(text)
+    for timex in found:
+        timex_found.append(timex)
+
+    # Year
+    found = reg5.findall(text)
+    for timex in found:
+        timex_found.append(timex)
