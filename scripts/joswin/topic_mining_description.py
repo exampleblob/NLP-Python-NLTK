a54bc52670fe56a5b0866a116fb15c69f255428e
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-02 08:25:40 +0000

    test image

--- scripts/joswin/topic_mining_description.py
+++ scripts/joswin/topic_mining_description.py
@@ -2,4 +2,6 @@ __author__ = 'joswin'
 
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:postgres@localhost:5432/linke
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/linkedin_data')
+
+tmp = pd.read_sql('linkedin_compan
