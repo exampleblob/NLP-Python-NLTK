f381926e40c9a60caebd2ccd08b6c8082a8fa7ea
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-05-02 06:40:56 +0000

    Remove console.logs

--- scripts/joswin/topic_mining_description.py
+++ scripts/joswin/topic_mining_description.py
@@ -2,4 +2,4 @@ __author__ = 'joswin'
 
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine('postgresql
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/linke
