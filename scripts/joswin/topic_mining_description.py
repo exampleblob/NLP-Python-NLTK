196f91d4bc62b3e4628379d056b95e280f4b1475
exampleblob <127121150+exampleblob@users.noreply.github.com>
2023-07-12 15:59:49 +0000

    usr corehq

--- scripts/joswin/topic_mining_description.py
+++ scripts/joswin/topic_mining_description.py
@@ -37,4 +37,5 @@ phr_list = pe.extract_phrase_treelistinput(grammar_list,['NP','PP','VP','CLAUSE'
 phr_list_all = [j for i in phr_list for j in i]
 phr_list_all = list(set(phr_list_all))
 
-pm = e
+pm = extract_phrases.PhraseMerger()
+
